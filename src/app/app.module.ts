f459af97ff707728b5554c126ae3e1a69cbecaa4
drdevn <devnnick@gmail.com>
2018-07-06 15:39:59 +0300

    footer component/error-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
+import { StaticModule } from './static/static.module';
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StaticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { StaticRoutingModule } from './static-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
   { path: 'statics', component: HeaderComponent }
+  { path: '**', component: ErrorPageComponent }
 ]
 
 @NgModule({
@@ -14,6 +17,13 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
   ],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent,
+     FooterComponent,
+     ErrorPageComponent,],
+
+  exports: [HeaderComponent,
+     FooterComponent,
+  ]
 })
+
 export class StaticModule { }
